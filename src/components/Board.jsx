1c96a813578aa2300b73699074c21896a30413c9
VIGNESH SANKAR A <va5140@srmist.edu.in>
2023-01-28 00:40:05 +0530

    drawMessage&pretty

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import Square from './Square'
-import "../styles/root.scss"
+import React from 'react';
+import Square from './Square';
+import '../styles/root.scss';
 
-const Board = ({board,handleSquareClick}) => {
-
-  const renderSquare =(position)=>{
-    return(
-      <Square value={board[position]} onClick={()=>{handleSquareClick(position) }}/>
+const Board = ({ board, handleSquareClick }) => {
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
     );
-
   };
 
   return (
-    <div className='board'>
-      <div className='board-row'>
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-      <div className='board-row'>
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
-      <div className='board-row'>
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
 };
 
-export default Board
+export default Board;
