edfe8ea0adfe40d825ea8ce8cf10f4f43a1e3ce2
VIGNESH SANKAR A <va5140@srmist.edu.in>
2023-01-28 19:27:55 +0530

    completed!

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -2,16 +2,36 @@ import React from 'react'
 
 const StatusMessage = ({winner,current}) => {
 
-    // const message = winner
-    // ? `Winner is ${winner}`
-    // : `Next Player is ${current.isXNext ? 'X' : 'O'}`;
+
     const noMovesLeft=current.board.every(el => el !== null);
 
-    return<h2>
-        {winner&& `winner is ${winner}`}
-        {!winner && !noMovesLeft&& `Next player is ${current.isXNext?'X':'O'}`}
-        {!winner && noMovesLeft && `X and O tied`} 
-    </h2>;
+    return(
+    <div className='status-message'>
+        {winner&& (
+            <>
+                winner is {' '}
+                <span className= {winner=== 'X' ? 'text-green':'text-orange'}>
+                    {winner}
+                </span>
+            </>
+        )}
+
+        {!winner && !noMovesLeft && (
+            <> 
+                Next player is{' '}
+                <span className= {current.isXNext? 'text-green' : 'text-orange'}>
+                    {current.isXNext ? 'X': 'O'}{' '}
+                </span>
+            </>
+        )}
+        {!winner && noMovesLeft && (
+            <>
+                <span className='text-green'>X</span> and{' '}
+                <span className='text-orange'>O</span> tied
+            </>
+        )} 
+    </div>
+    );
 };
 
 export default StatusMessage
